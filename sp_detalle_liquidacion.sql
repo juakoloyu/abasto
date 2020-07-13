4396917f9a3b9ea2cccbe966181b6a43f281c237
Jose Joaquin Logiudice <juakoloyu@gmail.com>
2020-07-13 17:34:20 -0300

    Delete sp_detalle_liquidacion.sql

--- sp_detalle_liquidacion.sql
+++
@@ -1,8 +0,0 @@
-create
-    definer = u283508441_rooter@`%` procedure sp_detalle_liquidacion_alimenticios(IN ingresoLiquidacion_id int, IN proviene int)
-BEGIN
-    INSERT INTO ingresos_liquidaciones_detalle (ingreso_liquidacion_id,impuesto_id,monto_impuesto)
-    SELECT ingresoLiquidacion_id,id,if(proviene=1,I.sp,if(proviene=2,I.otro_municipio,I.otra_provincia))
-    FROM impuestos I
-    WHERE tipo=1;
-end;
